577536b498209ae83c6043e71ca1148d33ffcdcd
Roverp <yar.zubaha@proton.me>
2025-06-17 00:26:30 +0200

    feat(vite.config): explicitly declare port to 9999

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 9999,
+    strictPort: true,
+  },
+
   plugins: [react(), tailwindcss()],
 });
