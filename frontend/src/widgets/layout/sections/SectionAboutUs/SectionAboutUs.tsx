4043f26975f271872529301924ec3313e6e42a09
Roverp <yar.zubaha@proton.me>
2025-06-20 23:44:01 +0200

    style(SectionAboutUs): initial styling of the section

--- frontend/src/widgets/layout/sections/SectionAboutUs/SectionAboutUs.tsx
+++ frontend/src/widgets/layout/sections/SectionAboutUs/SectionAboutUs.tsx
@@ -1,13 +1,23 @@
+import classNames from "classnames";
+
 import web_agency_author from "../../../../app/img/web_agency_author.avif";
 
 const SectionAboutUs = () => {
   return (
-    <section className="flex flex-col">
-      <span className="label">About Us</span>
-      <img
-        src={web_agency_author}
-        alt="Aster Craft author"
-      />
+    <section className={classNames("container flex flex-col pt-8 pb-16")}>
+      <div className="flex items-start justify-between">
+        <div className="flex items-center gap-1">
+          <span className="size-2 bg-black"></span>
+          <h2 className="font-normal uppercase">About Us</h2>
+        </div>
+
+        <img
+          src={web_agency_author}
+          alt="Aster Craft author"
+          // make css clamp() for the image
+          className="w-[35vw] max-w-[180px] min-w-[100px] rounded-lg"
+        />
+      </div>
     </section>
   );
 };
