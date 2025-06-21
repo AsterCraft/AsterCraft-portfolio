1b01e7c12f5f9ddac365e92fe4a468b1a37bbbeb
Roverp <yar.zubaha@proton.me>
2025-06-21 13:13:11 +0200

    layout(SectionAboutUs): write second part of the section

--- frontend/src/widgets/layout/sections/SectionAboutUs/SectionAboutUs.tsx
+++ frontend/src/widgets/layout/sections/SectionAboutUs/SectionAboutUs.tsx
@@ -5,10 +5,11 @@ import web_agency_author from "../../../../app/img/web_agency_author.avif";
 const SectionAboutUs = () => {
   return (
     <section className={classNames("container flex flex-col pt-8 pb-16")}>
-      <div className="flex items-start justify-between">
+      <div className="mb-16 flex items-start justify-between">
         <div className="flex items-center gap-1">
+          {/* black square */}
           <span className="size-2 bg-black"></span>
-          <h2 className="font-normal uppercase">About Us</h2>
+          <h3 className="font-normal uppercase">About Us</h3>
         </div>
 
         <img
@@ -18,6 +19,15 @@ const SectionAboutUs = () => {
           className="w-[35vw] max-w-[180px] min-w-[100px] rounded-lg"
         />
       </div>
+
+      <h2 className="mb-6 text-2xl">Hi, we’re Aster Craft</h2>
+      <p className="text-lg text-zinc-600">
+        A small web studio from Łódź. <br />
+        We design and build modern websites using React, TypeScript, and clean
+        code. <br />
+        Every project is fast, focused, and fully tailored to the client — from
+        personal sites to full business solutions.
+      </p>
     </section>
   );
 };
