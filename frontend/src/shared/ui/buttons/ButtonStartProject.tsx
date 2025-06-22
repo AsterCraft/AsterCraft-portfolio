73f366857734d7ec63e5e423b5df83f19c23993e
Roverp <yar.zubaha@proton.me>
2025-06-22 22:59:20 +0200

    feat(ButtonStartProject): create aciton button to start a projec

---
+++ frontend/src/shared/ui/buttons/ButtonStartProject.tsx
@@ -0,0 +1,9 @@
+const ButtonStartProject = () => {
+  return (
+    <button className="inline-block w-fit cursor-pointer rounded-md bg-(--ac-color-btn-action) px-8 py-4 text-lg">
+      Start a project
+    </button>
+  );
+};
+
+export default ButtonStartProject;
--- frontend/src/widgets/layout/sections/SectionAboutUs/SectionAboutUs.tsx
+++ frontend/src/widgets/layout/sections/SectionAboutUs/SectionAboutUs.tsx
@@ -1,10 +1,11 @@
 import classNames from "classnames";
 
 import web_agency_author from "../../../../app/img/web_agency_author.avif";
+import ButtonStartProject from "../../../../shared/ui/buttons/ButtonStartProject";
 
 const SectionAboutUs = () => {
   return (
-    <section className={classNames("Container flex flex-col pt-8 pb-16")}>
+    <section className="Container flex flex-col pt-8 pb-16">
       <div className="mb-16 flex items-start justify-between">
         <div className="flex items-center gap-1">
           {/* black square */}
@@ -20,16 +21,18 @@ const SectionAboutUs = () => {
         />
       </div>
 
-      <h2 className="mb-6 text-2xl">Hi, we’re Aster Craft</h2>
+      <h2 className="mb-6 text-2xl">Hi, we’re Aster Craft.</h2>
       <p className="text-lg text-zinc-600">
         A small web studio from Łódź. <br />
         We design and build modern websites using React, TypeScript, and clean
         code.
       </p>
-      <p className="text-lg text-zinc-600">
+      <p className="mb-9 text-lg text-zinc-600">
         Every project is fast, focused, and fully tailored to the client — from
         personal sites to full business solutions.
       </p>
+
+      <ButtonStartProject />
     </section>
   );
 };
