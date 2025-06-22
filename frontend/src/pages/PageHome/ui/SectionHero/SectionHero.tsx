887d2284533fb846beec3b42474aa7ea07126f22
MEDOYED <mmax26072005@gmail.com>
2025-06-22 16:03:18 +0200

    style(SectionHero.tsx): media

--- frontend/src/pages/PageHome/ui/SectionHero/SectionHero.tsx
+++ frontend/src/pages/PageHome/ui/SectionHero/SectionHero.tsx
@@ -1,15 +1,14 @@
 const SectionHero = () => {
   return (
     <section className="h-[100vh] w-full bg-[#1a1a1a] pt-20 text-white">
-      {/* text-8xl --> for more 640px screen */}
-      <h1 className="pt-35 pl-10 text-6xl">Aster</h1>
-
-      {/* text-8xl pl-40 --> for more 640px screen */}
-      <h2 className="pl-27 text-6xl">Craft</h2>
+      <div className="pt-35 pl-10 text-6xl sm:text-8xl md:text-[10rem]">
+        <h1 className="">Aster</h1>
+        <h2 className="pl-17 sm:pl-40">Craft</h2>
+      </div>
 
       <hr className="mt-50 mb-10 text-gray-500" />
 
-      <div className="flex flex-col justify-between sm:flex-row">
+      <div className="Container flex flex-col justify-between sm:flex-row">
         <p>I help you make a lasting impression online.</p>
         <button>Start project</button>
       </div>
