e494afe5acd4655946c7a48159f6cf912700ffcc
MEDOYED <mmax26072005@gmail.com>
2025-06-22 19:52:11 +0200

    feat(IconBurger.tsx): create a animated burger icon

--- frontend/src/widgets/layout/Header/BurgerDropdownMenu/BurgerDropdownMenu.tsx
+++ frontend/src/widgets/layout/Header/BurgerDropdownMenu/BurgerDropdownMenu.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
+import IconBurger from "../IconBurger/IconBurger";
+
 const BurgerDropdownMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
-  console.log(isOpen);
 
   return (
     <button className="relative" onClick={() => setIsOpen((prev) => !prev)}>
-      <span>X</span>
+      <IconBurger isOpen={isOpen} />
 
       {isOpen && (
         <ul className="absolute top-12 right-[-7px] rounded-md bg-black pt-5 pr-5 pb-5 pl-5 text-2xl">
