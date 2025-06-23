79be2af6ccabf1c2be8b401877ccb264d1cf98ab
Roverp <yar.zubaha@proton.me>
2025-06-23 23:43:07 +0200

    style(SectionAboutUs): initial styling for sm: breakpoint

--- frontend/src/widgets/layout/sections/SectionAboutUs/SectionAboutUs.tsx
+++ frontend/src/widgets/layout/sections/SectionAboutUs/SectionAboutUs.tsx
@@ -1,11 +1,23 @@
+import classNames from "classnames";
+
 import ButtonStartProject from "../../../../shared/ui/buttons/ButtonStartProject";
 
 import web_agency_author from "../../../../app/img/web_agency_author.avif";
 
 const SectionAboutUs = () => {
   return (
-    <section className="Container flex flex-col pt-8 pb-16">
-      <div className="mb-16 flex items-start justify-between">
+    <section
+      className={classNames(
+        "Container flex flex-col gap-18 pt-8 pb-16",
+        "sm:flex-row sm:gap-[15%]"
+      )}
+    >
+      <div
+        className={classNames(
+          "flex items-start justify-between",
+          "sm:flex-col"
+        )}
+      >
         <div className="flex items-center gap-1">
           {/* black square */}
           <span className="size-2 bg-black"></span>
@@ -16,22 +28,24 @@ const SectionAboutUs = () => {
           src={web_agency_author}
           alt="Aster Craft author"
           // make css clamp() for the image
-          className="w-[35vw] max-w-[180px] min-w-[100px] rounded-lg"
+          className="w-[clamp(100px,35vw,170px)] rounded-lg"
         />
       </div>
 
-      <h2 className="mb-6 text-2xl">Hi, we’re Aster Craft.</h2>
-      <p className="text-lg text-zinc-600">
-        A small web studio from Łódź. <br />
-        We design and build modern websites using React, TypeScript, and clean
-        code.
-      </p>
-      <p className="mb-9 text-lg text-zinc-600">
-        Every project is fast, focused, and fully tailored to the client — from
-        personal sites to full business solutions.
-      </p>
+      <div className="flex flex-col">
+        <h2 className="mb-6 text-2xl">Hi, we’re Aster Craft.</h2>
+        <p className="text-lg text-zinc-600">
+          A small web studio from Łódź. <br />
+          We design and build modern websites using React, TypeScript, and clean
+          code.
+        </p>
+        <p className="mb-9 text-lg text-zinc-600">
+          Every project is fast, focused, and fully tailored to the client —
+          from personal sites to full business solutions.
+        </p>
 
-      <ButtonStartProject />
+        <ButtonStartProject />
+      </div>
     </section>
   );
 };
