6e516ad3af2c509bb4cd473f88b8d2139caa6b87
MEDOYED <mmax26072005@gmail.com>
2025-06-26 16:26:53 +0200

    style(BulletBlackSquare.tsx)

--- frontend/src/shared/ui/bullets/BulletBlackSquare/BulletBlackSquare.tsx
+++ frontend/src/shared/ui/bullets/BulletBlackSquare/BulletBlackSquare.tsx
@@ -1,6 +1,5 @@
 const BulletBlackSquare = () => {
-  return <span className="inline-block size-2 bg-black"></span>;
-  // return <span className="mt-1js mr-2 inline-block h-2 w-2 bg-black"></span>;
+  return <span className="mr-2 inline-block size-2 bg-black"></span>;
 };
 
 export default BulletBlackSquare;
