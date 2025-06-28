11961872549eb06e5730d0e6b5c83c573e5357af
MEDOYED <mmax26072005@gmail.com>
2025-06-28 17:04:19 +0200

    feat(SectionProjects.tsx): media adaptation and filled in 1 project card of real data

--- frontend/src/shared/data/dataProjects.ts
+++ frontend/src/shared/data/dataProjects.ts
@@ -1,4 +1,5 @@
 type Projects = {
+  url: string;
   imgBg: string;
   imgWebsite: string;
   title: string;
@@ -7,24 +8,28 @@ type Projects = {
 
 const dataProjects: Projects[] = [
   {
+    url: "https://www.uniprint.lviv.ua/",
     imgBg: "/img/projects/bg/1.png",
     imgWebsite: "/img/projects/website/1.png",
-    title: "Meet your mentor",
-    subtitle: "Website for content creator Simo Azzaoui",
+    title: "Uni Print",
+    subtitle: "Website for Uni Print — visual ads, banners, signs, car wraps.",
   },
   {
+    url: "",
     imgBg: "/img/projects/bg/2.png",
     imgWebsite: "/img/projects/website/2.png",
     title: "Recording Studio B12",
     subtitle: "Website in cooperation with 'Your Coach'",
   },
   {
+    url: "",
     imgBg: "/img/projects/bg/3.png",
     imgWebsite: "/img/projects/website/3.png",
     title: "Your reasons",
     subtitle: "Website for a Gestalt therapist",
   },
   {
+    url: "",
     imgBg: "/img/projects/bg/4.png",
     imgWebsite: "/img/projects/website/4.png",
     title: "Smart Chiefs",
--- frontend/src/widgets/sections/SectionProjects/SectionProjects.tsx
+++ frontend/src/widgets/sections/SectionProjects/SectionProjects.tsx
@@ -1,3 +1,5 @@
+import classNames from "classnames";
+
 import BulletBlackSquare from "../../../shared/ui/bullets/BulletBlackSquare/BulletBlackSquare";
 
 import dataProjects from "../../../shared/data/dataProjects";
@@ -5,14 +7,24 @@ import dataProjects from "../../../shared/data/dataProjects";
 const SectionProjects = () => {
   return (
     <section className="Container my-15">
-      <h2>
+      <h2 className="mb-8">
         <BulletBlackSquare />
         <span className="uppercase">projects</span>
       </h2>
 
-      <section>
+      <section
+        className={classNames(
+          "grid grid-cols-1 gap-5",
+          "md:grid-cols-2",
+          "xl:gap-10"
+        )}
+      >
         {dataProjects.map((project, index) => (
-          <article key={index}>
+          <a
+            href={project.url}
+            target="_blank"
+            key={index}
+          >
             <div className="w-full] relative h-[300px]">
               {/* image wrapper */}
               <img
@@ -21,7 +33,7 @@ const SectionProjects = () => {
                 alt={project.subtitle}
               />
               <img
-                className="absolute top-1/2 left-1/2 max-h-[70%] max-w-[80%] -translate-x-1/2 -translate-y-1/2 transform"
+                className="absolute top-1/2 left-1/2 max-h-[70%] max-w-[80%] min-w-[300px] -translate-x-1/2 -translate-y-1/2 transform rounded-lg"
                 src={project.imgWebsite}
                 alt={project.subtitle}
               />
@@ -30,7 +42,7 @@ const SectionProjects = () => {
               <h3 className="text-xl font-medium">{project.title}</h3>
               <h4 className="text-zinc-600">{project.subtitle}</h4>
             </div>
-          </article>
+          </a>
         ))}
       </section>
     </section>
