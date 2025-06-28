fda67d764bb49331f316846dfc0cce53ce97e084
MEDOYED <mmax26072005@gmail.com>
2025-06-28 17:07:59 +0200

    chore: rename folder hrs to lines

--- frontend/src/pages/PageHome/PageHome.tsx
+++ frontend/src/pages/PageHome/PageHome.tsx
@@ -1,6 +1,6 @@
 import SectionAboutUs from "../../widgets/layout/sections/SectionAboutUs/SectionAboutUs";
 import SectionHero from "./ui/SectionHero/SectionHero";
-import DividerBetweenSections from "../../shared/ui/hrs/DividerBetweenSections";
+import DividerBetweenSections from "../../shared/ui/lines/DividerBetweenSections";
 import SectionProjects from "../../widgets/sections/SectionProjects/SectionProjects";
 import SliderTestimonials from "../../widgets/sliders/SliderTestimonials/SliderTestimonials";
 
