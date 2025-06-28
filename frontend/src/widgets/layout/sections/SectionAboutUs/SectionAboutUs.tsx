ee69340e7eac7c5321db1e77f6d5156898f4b037
Roverp <yar.zubaha@proton.me>
2025-06-28 17:58:33 +0200

    fix(SectionAboutUs): remove extra black square

--- frontend/src/widgets/layout/sections/SectionAboutUs/SectionAboutUs.tsx
+++ frontend/src/widgets/layout/sections/SectionAboutUs/SectionAboutUs.tsx
@@ -10,7 +10,7 @@ const SectionAboutUs = () => {
     <section
       className={classNames(
         "Container flex flex-col gap-18 pt-8 pb-16",
-        "sm:flex-row",
+        "sm:flex-row"
       )}
     >
       <div
@@ -21,8 +21,9 @@ const SectionAboutUs = () => {
       >
         <div className="flex items-center gap-1">
           <BulletBlackSquare />
-          <span className="size-2 bg-black"></span>
-          <h3 className="font-normal uppercase text-[clamp(1rem,0.6vw+0.5rem,1.4rem)]">About Us</h3>
+          <h3 className="text-[clamp(1rem,0.6vw+0.5rem,1.4rem)] font-normal uppercase">
+            About Us
+          </h3>
         </div>
 
         <img
@@ -31,22 +32,25 @@ const SectionAboutUs = () => {
           // simulate css clamp() for the image
           // clamp() as an arbitrary value doesnt work properly for width with multiple breakpoints
           className={classNames(
-            "min-w-15 w-[30vw] max-w-39 rounded-lg", 
-            "sm:min-w-25 sm:w-[15vw]"
+            "w-[30vw] max-w-39 min-w-15 rounded-lg",
+            "sm:w-[15vw] sm:min-w-25"
           )}
         />
       </div>
 
       {/* container with text */}
-      <div className="flex flex-col max-w-160 mx-auto">
-        <h2 className={classNames(
-          "mb-6 text-[clamp(1.5rem,5.5vw+0.25rem,1.75rem)] leading-snug",
-          "sm:text-[clamp(1.75rem,3.5vw+0.25rem,2.25rem)]",
-          "lg:text-[clamp(2.25rem,2.5vw+0.25rem,2.75rem)]"
-        )}>
-          Hi, we’re Aster Craft.<br/>
-          A small web studio from Łódź.</h2>
-        
+      <div className="mx-auto flex max-w-160 flex-col">
+        <h2
+          className={classNames(
+            "mb-6 text-[clamp(1.5rem,5.5vw+0.25rem,1.75rem)] leading-snug",
+            "sm:text-[clamp(1.75rem,3.5vw+0.25rem,2.25rem)]",
+            "lg:text-[clamp(2.25rem,2.5vw+0.25rem,2.75rem)]"
+          )}
+        >
+          Hi, we’re Aster Craft.
+          <br />A small web studio from Łódź.
+        </h2>
+
         <p className="text-lg text-zinc-600">
           We design and build modern websites using React, TypeScript, and clean
           code.
