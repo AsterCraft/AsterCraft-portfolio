df80eda92915f7f349ac429210d9755bf5d4b042
MEDOYED <mmax26072005@gmail.com>
2025-06-30 18:05:42 +0200

    feat(HeroTitle.tsx): add animations on start page

--- frontend/src/pages/PageHome/SectionHero/HeroTitle/HeroTitle.tsx
+++ frontend/src/pages/PageHome/SectionHero/HeroTitle/HeroTitle.tsx
@@ -1,8 +1,24 @@
+import { motion } from "motion/react";
+
 const HeroTitle = () => {
   return (
     <h1 className="w-fit text-8xl sm:text-8xl lg:text-[10rem]">
-      <span className="block">Aster</span>
-      <span className="block pl-10 lg:pl-30">Craft</span>
+      <motion.span
+        initial={{ x: "-400px" }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.5, ease: [0, 0.1, 0.4, 1] }}
+        className="block"
+      >
+        Aster
+      </motion.span>
+      <motion.span
+        initial={{ x: "-300px" }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.5, delay: 0.1, ease: [0, 0.1, 0.4, 1] }}
+        className="block pl-10 lg:pl-30"
+      >
+        Craft
+      </motion.span>
     </h1>
   );
 };
