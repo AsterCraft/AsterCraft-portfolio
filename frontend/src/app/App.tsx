213d6c0fd9a4350aea900cb61ef1e0660df9ea9a
Roverp <yar.zubaha@proton.me>
2025-07-02 22:21:06 +0200

    feat(App): provide ContextModalStartProject

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,3 +1,4 @@
+import { ProviderModalStartProject } from "../shared/store/ContextModalStartProject";
 import Header from "../widgets/layout/Header/Header";
 import PageHome from "../pages/PageHome/PageHome";
 
@@ -8,10 +9,12 @@ function App() {
     // container
     // <div className="mx-auto max-w-[1450px] px-[10px]">
     // add bottom padding to test inView animations
-    <div className="pb-200">
-      <Header />
-      <PageHome />
-    </div>
+    <ProviderModalStartProject>
+      <div className="pb-200">
+        <Header />
+        <PageHome />
+      </div>
+    </ProviderModalStartProject>
   );
 }
 
