bf9a449af2cb35f66c4226013c700d640c552236
MEDOYED <mmax26072005@gmail.com>
2025-07-04 13:23:19 +0200

    refactord(ButtonStartProject.tsx): remove wrapper of button

--- frontend/src/shared/ui/buttons/ButtonStartProject/ButtonStartProject.tsx
+++ frontend/src/shared/ui/buttons/ButtonStartProject/ButtonStartProject.tsx
@@ -5,11 +5,11 @@ type ButtonStartProjectProps = {
 
 const ButtonStartProject = ({ className, text }: ButtonStartProjectProps) => {
   return (
-    <div className={className}>
-      <button className="bg-ac-btn-action inline-block w-fit cursor-pointer rounded-md px-8 py-4 text-lg whitespace-nowrap text-black">
-        {text || "Start a project"}
-      </button>
-    </div>
+    <button
+      className={`bg-ac-btn-action ${className} inline-block w-fit cursor-pointer rounded-md px-8 py-4 text-lg whitespace-nowrap text-black`}
+    >
+      {text || "Start a project"}
+    </button>
   );
 };
 
