f7cdbaf71018fed5a67b004438a6f1a537e17ed1
Roverp <yar.zubaha@proton.me>
2025-07-14 19:13:54 +0200

    style(ModalStartProject): add blur to the background and animation for it

--- frontend/src/widgets/modals/ModalStartProject/ModalStartProject.tsx
+++ frontend/src/widgets/modals/ModalStartProject/ModalStartProject.tsx
@@ -1,5 +1,7 @@
 import classNames from "classnames";
+import { motion, AnimatePresence } from "motion/react";
 import { useContext } from "react";
+
 import ContextModalStartProject from "../../../shared/store/ContextModalStartProject";
 
 const ModalStartProject = () => {
@@ -12,193 +14,198 @@ const ModalStartProject = () => {
 
   return (
     <>
-      {isOpen && (
-        <div className={classNames("fixed inset-0 z-[1111] overflow-scroll")}>
-          <div
-            className={classNames(
-              "hidden",
-              "lg:fixed lg:inset-y-0 lg:left-0 lg:block lg:w-1/2 lg:bg-black/50 lg:text-white"
-            )}
-            onClick={() => setIsOpen(false)}
-          ></div>
+      <AnimatePresence>
+        {isOpen && (
+          <div className={classNames("fixed inset-0 z-[1111] overflow-scroll")}>
+            <motion.div
+              initial={{ backdropFilter: "blur(0px)", opacity: 0 }}
+              animate={{ backdropFilter: "blur(8px)", opacity: 1 }}
+              exit={{ backdropFilter: "blur(0px)", opacity: 0 }}
+              className={classNames(
+                "hidden",
+                "lg:fixed lg:inset-y-0 lg:left-0 lg:block lg:w-1/2 lg:bg-black/50 lg:text-white"
+              )}
+              onClick={() => setIsOpen(false)}
+            ></motion.div>
 
-          <div
-            className={classNames(
-              "fixed inset-y-0 right-0 w-full bg-white p-6",
-              "lg:w-1/2"
-            )}
-          >
-            <div className="mt-6 mb-12 flex justify-end">
-              <button
-                onClick={() => setIsOpen(false)}
-                className="text-xl"
-              >
-                Close
-              </button>
-            </div>
-
-            <header className="mb-16">
-              <h2
-                className={classNames(
-                  "mb-4 text-[clamp(1.6rem,3.2vw+0.4rem,2rem)]",
-                  "lg:text-[clamp(1.6rem,2vw+0.4rem,2.3rem)]"
-                )}
-              >
-                Start a project
-              </h2>
-              <p
-                className={classNames(
-                  "text-ac-paragraph-light text-[clamp(1.2rem,1.9vw+0.4rem,1.4rem)]",
-                  "lg:text-[clamp(1.3rem,1vw+0.4rem,1.4rem)]"
-                )}
-              >
-                Tell me a bit about your project! Just fill out the form or drop
-                me an{" "}
-                <a
-                  href="#"
-                  className="text-ac-link-email underline"
+            <motion.div
+              className={classNames(
+                "fixed inset-y-0 right-0 w-full bg-white p-6",
+                "lg:w-1/2"
+              )}
+            >
+              <div className="mt-6 mb-12 flex justify-end">
+                <button
+                  onClick={() => setIsOpen(false)}
+                  className="text-xl"
                 >
-                  E-Mail
-                </a>{" "}
-                — I’ll get back to you as soon as possible.
-              </p>
-            </header>
+                  Close
+                </button>
+              </div>
 
-            <form className={classNames("flex flex-col gap-y-4")}>
-              {/* each grid inside form represents a single row inside form */}
-              <div
-                className={classNames(
-                  "grid grid-cols-1 gap-x-4 gap-y-4",
-                  "sm:grid-cols-2"
-                )}
-              >
-                {/* each div inside <form> row is a wrapper for <label> + <input> */}
-                <div
+              <header className="mb-16">
+                <h2
                   className={classNames(
-                    "modal-start-project__field-wrapper",
-                    "bg-ac-input-start-projec flex flex-col rounded-md px-5 pt-4 pb-3"
+                    "mb-4 text-[clamp(1.6rem,3.2vw+0.4rem,2rem)]",
+                    "lg:text-[clamp(1.6rem,2vw+0.4rem,2.3rem)]"
                   )}
                 >
-                  <label
-                    htmlFor="first-name"
-                    className="text-sm"
-                  >
-                    First Name
-                  </label>
-                  <input
-                    type="text"
-                    id="first-name"
-                    placeholder="Maksym"
-                    className="h-9 outline-none"
-                  />
-                </div>
-
-                <div
+                  Start a project
+                </h2>
+                <p
                   className={classNames(
-                    "modal-start-project__field-wrapper",
-                    "bg-ac-input-start-projec flex flex-col rounded-md px-5 pt-4 pb-3"
+                    "text-ac-paragraph-light text-[clamp(1.2rem,1.9vw+0.4rem,1.4rem)]",
+                    "lg:text-[clamp(1.3rem,1vw+0.4rem,1.4rem)]"
                   )}
                 >
-                  <label
-                    htmlFor="last-name"
-                    className="text-sm"
-                  >
-                    Last Name
-                  </label>
-                  <input
-                    type="text"
-                    id="last-name"
-                    placeholder="Mokriakov"
-                    className="h-9 outline-none"
-                  />
-                </div>
-
-                <div
-                  className={classNames(
-                    "modal-start-project__field-wrapper",
-                    "bg-ac-input-start-projec flex flex-col rounded-md px-5 pt-4 pb-3"
-                  )}
-                >
-                  <label
-                    htmlFor="e-mail"
-                    className="text-sm"
+                  Tell me a bit about your project! Just fill out the form or
+                  drop me an{" "}
+                  <a
+                    href="#"
+                    className="text-ac-link-email underline"
                   >
                     E-Mail
-                  </label>
-                  <input
-                    type="email"
-                    id="e-mail"
-                    placeholder="me@gmail.com"
-                    className="h-9 outline-none"
-                  />
-                </div>
+                  </a>{" "}
+                  — I’ll get back to you as soon as possible.
+                </p>
+              </header>
 
+              <form className={classNames("flex flex-col gap-y-4")}>
+                {/* each grid inside form represents a single row inside form */}
                 <div
                   className={classNames(
-                    "modal-start-project__field-wrapper",
-                    "bg-ac-input-start-projec flex flex-col rounded-md px-5 pt-4 pb-3"
+                    "grid grid-cols-1 gap-x-4 gap-y-4",
+                    "sm:grid-cols-2"
                   )}
                 >
-                  <label
-                    htmlFor="Phone"
-                    className="text-sm"
+                  {/* each div inside <form> row is a wrapper for <label> + <input> */}
+                  <div
+                    className={classNames(
+                      "modal-start-project__field-wrapper",
+                      "bg-ac-input-start-projec flex flex-col rounded-md px-5 pt-4 pb-3"
+                    )}
                   >
-                    Phone
-                  </label>
-                  <input
-                    type="text"
-                    id="phone"
-                    placeholder="+48790833877"
-                    className="h-9 outline-none"
-                  />
-                </div>
-              </div>
+                    <label
+                      htmlFor="first-name"
+                      className="text-sm"
+                    >
+                      First Name
+                    </label>
+                    <input
+                      type="text"
+                      id="first-name"
+                      placeholder="Maksym"
+                      className="h-9 outline-none"
+                    />
+                  </div>
 
-              <div className="grid grid-cols-1 gap-y-4">
-                <div
-                  className={classNames(
-                    "modal-start-project__field-wrapper",
-                    "bg-ac-input-start-projec flex flex-col rounded-md px-5 pt-4 pb-3"
-                  )}
-                >
-                  <label
-                    htmlFor="Project-Details"
-                    className="text-sm"
+                  <div
+                    className={classNames(
+                      "modal-start-project__field-wrapper",
+                      "bg-ac-input-start-projec flex flex-col rounded-md px-5 pt-4 pb-3"
+                    )}
                   >
-                    Project Details
-                  </label>
-                  <textarea
-                    name="Project_Details"
-                    id="Project-Details"
-                    className={textAreaStyles}
-                    placeholder="Describe your project"
-                  ></textarea>
+                    <label
+                      htmlFor="last-name"
+                      className="text-sm"
+                    >
+                      Last Name
+                    </label>
+                    <input
+                      type="text"
+                      id="last-name"
+                      placeholder="Mokriakov"
+                      className="h-9 outline-none"
+                    />
+                  </div>
+
+                  <div
+                    className={classNames(
+                      "modal-start-project__field-wrapper",
+                      "bg-ac-input-start-projec flex flex-col rounded-md px-5 pt-4 pb-3"
+                    )}
+                  >
+                    <label
+                      htmlFor="e-mail"
+                      className="text-sm"
+                    >
+                      E-Mail
+                    </label>
+                    <input
+                      type="email"
+                      id="e-mail"
+                      placeholder="me@gmail.com"
+                      className="h-9 outline-none"
+                    />
+                  </div>
+
+                  <div
+                    className={classNames(
+                      "modal-start-project__field-wrapper",
+                      "bg-ac-input-start-projec flex flex-col rounded-md px-5 pt-4 pb-3"
+                    )}
+                  >
+                    <label
+                      htmlFor="Phone"
+                      className="text-sm"
+                    >
+                      Phone
+                    </label>
+                    <input
+                      type="text"
+                      id="phone"
+                      placeholder="+48790833877"
+                      className="h-9 outline-none"
+                    />
+                  </div>
                 </div>
 
-                <div
-                  className={classNames(
-                    "modal-start-project__field-wrapper",
-                    "bg-ac-input-start-projec flex flex-col rounded-md px-5 pt-4 pb-3"
-                  )}
-                >
-                  <label
-                    htmlFor="Message"
-                    className="text-sm"
+                <div className="grid grid-cols-1 gap-y-4">
+                  <div
+                    className={classNames(
+                      "modal-start-project__field-wrapper",
+                      "bg-ac-input-start-projec flex flex-col rounded-md px-5 pt-4 pb-3"
+                    )}
                   >
-                    Project Details
-                  </label>
-                  <textarea
-                    name="Message"
-                    id="Message"
-                    className={textAreaStyles}
-                    placeholder="Any additional notes?"
-                  ></textarea>
+                    <label
+                      htmlFor="Project-Details"
+                      className="text-sm"
+                    >
+                      Project Details
+                    </label>
+                    <textarea
+                      name="Project_Details"
+                      id="Project-Details"
+                      className={textAreaStyles}
+                      placeholder="Describe your project"
+                    ></textarea>
+                  </div>
+
+                  <div
+                    className={classNames(
+                      "modal-start-project__field-wrapper",
+                      "bg-ac-input-start-projec flex flex-col rounded-md px-5 pt-4 pb-3"
+                    )}
+                  >
+                    <label
+                      htmlFor="Message"
+                      className="text-sm"
+                    >
+                      Project Details
+                    </label>
+                    <textarea
+                      name="Message"
+                      id="Message"
+                      className={textAreaStyles}
+                      placeholder="Any additional notes?"
+                    ></textarea>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </motion.div>
           </div>
-        </div>
-      )}
+        )}
+      </AnimatePresence>
     </>
   );
 };
