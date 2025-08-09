6b34cefaee99223c9dc5aea4716d5087543a7afd
MEDOYED <mmax26072005@gmail.com>
2025-08-09 22:08:15 +0200

    feat(SectionDevelopmentProcess): media for all screens

--- frontend/src/widgets/sections/SectionDevelopmentProcess/ui/SectionDevelopmentProcess.tsx
+++ frontend/src/widgets/sections/SectionDevelopmentProcess/ui/SectionDevelopmentProcess.tsx
@@ -11,52 +11,80 @@ const SectionDevelopmentProcess = () => {
         <TitleSection title="Процес створення сайту" />
 
         <ul className="grid gap-15">
-          <li className="grid grid-cols-[auto_1fr] gap-7">
+          {/* ================ stage={1} ======================================== */}
+
+          <li
+            className={cn(
+              "grid grid-cols-[auto_1fr] gap-7",
+              "md:grid-cols-[1fr_auto_1fr]"
+            )}
+          >
             <IconStage
-              className="order-1"
+              className="order-1 md:order-2"
               dots={1}
             />
             <StageDescription
               stage={1}
-              className="order-2"
+              className="order-2 md:order-3"
             />
-            <div className="hidden"></div>
+            <div className={cn("hidden", "md:order-1 md:block")}></div>
           </li>
 
-          <li className="grid grid-cols-[auto_1fr] gap-7">
+          {/* ===================== stage={2} =================================== */}
+
+          <li
+            className={cn(
+              "grid grid-cols-[auto_1fr] gap-7",
+              "md:grid-cols-[1fr_auto_1fr]"
+            )}
+          >
             <IconStage
-              className="order-1"
+              className="order-1 md:order-2"
               dots={2}
             />
             <StageDescription
               stage={2}
-              className="order-2"
+              className="order-2 md:order-1"
             />
-            <div className="hidden"></div>
+            <div className={cn("hidden", "md:order-3 md:block")}></div>
           </li>
 
-          <li className="grid grid-cols-[auto_1fr] gap-7">
+          {/* ===================== stage={3} =================================== */}
+
+          <li
+            className={cn(
+              "grid grid-cols-[auto_1fr] gap-7",
+              "md:grid-cols-[1fr_auto_1fr]"
+            )}
+          >
             <IconStage
-              className="order-1"
+              className="order-1 md:order-2"
               dots={3}
             />
             <StageDescription
               stage={3}
-              className="order-2"
+              className="order-2 md:order-3"
             />
-            <div className="hidden"></div>
+            <div className={cn("hidden", "md:order-1 md:block")}></div>
           </li>
 
-          <li className="grid grid-cols-[auto_1fr] gap-7">
+          {/* =================== stage={4} ===================================== */}
+
+          <li
+            className={cn(
+              "grid grid-cols-[auto_1fr] gap-7",
+              "md:grid-cols-[1fr_auto_1fr]"
+            )}
+          >
             <IconStage
-              className="order-1"
+              className="order-1 md:order-2"
               dots={4}
             />
             <StageDescription
               stage={4}
-              className="order-2"
+              className="order-2 md:order-1"
             />
-            <div className="hidden"></div>
+            <div className={cn("hidden", "md:order-3 md:block")}></div>
           </li>
         </ul>
       </div>
@@ -65,37 +93,3 @@ const SectionDevelopmentProcess = () => {
 };
 
 export default SectionDevelopmentProcess;
-
-// import cn from "classnames";
-// import StageDescription from "./StageDescription";
-// import IconStage from "./IconStage";
-
-// const SectionDevelopmentProcess = () => {
-//   return (
-//     <section className={cn("", "bg-ac-eerie-black pt-10 text-white")}>
-//       <div className="app-container">
-//         <h2>Процес створення сайту</h2>
-//         <ul>
-//           <li className="grid grid-cols-[auto_1fr] items-center gap-4 md:grid-cols-[2fr_100px_2fr] md:gap-10">
-//             <div className="hidden md:block"></div>
-//             <IconStage
-//               className="order-1"
-//               dots={1}
-//             />
-//             <StageDescription className="order-2 bg-red-500" />
-//           </li>
-//           <li className="grid grid-cols-[auto_1fr] items-center gap-4 md:grid-cols-[2fr_100px_2fr] md:gap-10">
-//             <StageDescription className="order-2 bg-red-500 md:order-1" />
-//             <IconStage
-//               className="order-1 md:order-2"
-//               dots={1}
-//             />
-//             <div className="hidden md:order-3 md:block"></div>
-//           </li>
-//         </ul>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default SectionDevelopmentProcess;
