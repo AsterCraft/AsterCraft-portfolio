3737b400ee4d8192e9e2c6b541d246082232bd56
MEDOYED <mmax26072005@gmail.com>
2025-08-12 09:36:08 +0200

    feat(SectionConsultation): create and connect component

---
+++ frontend/src/widgets/sections/SectionConsultation/ui/SectionConsultation.tsx
@@ -0,0 +1,13 @@
+import TitleSection from "../../../../shared/ui/typography/TitleSection";
+
+const SectionConsultation = () => {
+  return (
+    <section>
+      <TitleSection title="Почніть з консультації — заповніть форму" />
+
+      <h3>Замовте безкоштовну консультацію, на якій ви одразу отримаєте:</h3>
+    </section>
+  );
+};
+
+export default SectionConsultation;
