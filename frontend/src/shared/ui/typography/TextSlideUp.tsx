24b0f6707cd928e883d64dabc82c54018dca9707
MEDOYED <mmax26072005@gmail.com>
2025-10-06 00:35:48 +0200

    feat(SectionAboutUs): revrite animation with new Component TextSlideUp

---
+++ frontend/src/shared/ui/typography/TextSlideUp.tsx
@@ -0,0 +1,53 @@
+import { stagger, useAnimate, useInView } from "motion/react";
+import { useEffect } from "react";
+import cn from "classnames";
+
+interface TextSlideUpProps {
+  text: string;
+  as: "h2" | "p";
+
+  // css
+  lineHeight?: string;
+}
+
+export const TextSlideUp = ({
+  text,
+  as: Component, // rename for Component for React jsx syntax
+  lineHeight = "leading-snug",
+}: TextSlideUpProps) => {
+  const [textRef, animate] = useAnimate();
+  const inView: boolean = useInView(textRef, { once: true, amount: 0.6 });
+
+  useEffect(() => {
+    if (!textRef.current) return;
+
+    if (inView) {
+      animate(textRef.current, { opacity: 1, y: 0 }, { duration: 0.6 });
+    } else {
+      // set initial element state
+      // from which animation will be played
+      animate(textRef.current, { opacity: 0, y: 24 }, { duration: 0 });
+    }
+  }, [inView, animate]);
+
+  let styles;
+  if (Component === "h2") {
+    styles = cn(
+      lineHeight,
+      "text-[clamp(1.5rem,5.5vw+0.25rem,1.75rem)]",
+      "sm:text-[clamp(1.75rem,3.5vw+0.25rem,2.25rem)]",
+      "lg:text-[clamp(2.25rem,2.5vw+0.25rem,2.75rem)]"
+    );
+  } else if (Component === "p") {
+    styles = cn("text-lg text-zinc-600");
+  }
+
+  return (
+    <Component
+      ref={textRef}
+      className={styles}
+    >
+      {text}
+    </Component>
+  );
+};
---
+++ frontend/src/shared/ui/typography/index.ts
@@ -0,0 +1,1 @@
+export { TextSlideUp } from "./TextSlideUp";
