f7e20b4d51ec9e74c35dc377bcfad06d5add3762
Roverp <yar.zubaha@proton.me>
2025-10-07 01:16:22 +0200

    feat(flake): write simple flake for development

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Aster Craft Portfolio dev enviroment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [nodejs];
+
+          shellHook = ''
+            echo "Node.js dev environment loaded"
+          '';
+        };
+      }
+    );
+}
