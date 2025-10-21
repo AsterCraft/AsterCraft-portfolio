7b2b28e1f52700de8acbfa9fd02ebd18a7a65d57
Roverp <yar.zubaha@proton.me>
2025-10-21 17:29:14 +0200

    feat(frontend): add log for testing

--- frontend/src/shared/config/api.ts
+++ frontend/src/shared/config/api.ts
@@ -1,6 +1,11 @@
 let apiBaseUrl = import.meta.env.VITE_API_URL;
 
-if (!apiBaseUrl) apiBaseUrl = "http://localhost:7979";
+if (!apiBaseUrl) {
+  console.warn("Failed to get VITE_API_URL env, using localhost");
+  apiBaseUrl = "http://localhost:7979";
+}
+
+console.log(`Using ${apiBaseUrl} as api url`);
 
 export const API_ENDPOINTS = {
   sendEmail: `${apiBaseUrl}/api/send-email`,
