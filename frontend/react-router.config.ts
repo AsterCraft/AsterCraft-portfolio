8846379204ce38be06c011b3c15b82423ceef3e6
Roverp <yar.zubaha@proton.me>
2025-10-25 11:19:04 +0200

    refactor(frontend): -> improve fsd structure

--- frontend/react-router.config.ts
+++ frontend/react-router.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "@react-router/dev/config";
 import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
-  appDirectory: "src",
+  appDirectory: "src/app",
   ssr: false,
   presets: [vercelPreset()],
 } satisfies Config;
--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -5,7 +5,7 @@ import ButtonStartProject from "@shared/ui/buttons/ButtonStartProject/ButtonStar
 
 import s from "./header.module.scss";
 
-const Header = () => {
+export const Header = () => {
   return (
     <header className="fixed top-0 right-0 left-0 z-50 text-xl">
       <div
@@ -64,5 +64,3 @@ const Header = () => {
     </header>
   );
 };
-
-export default Header;
