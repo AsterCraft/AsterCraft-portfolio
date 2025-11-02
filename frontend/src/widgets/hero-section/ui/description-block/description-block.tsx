0c8b283972043ad4bd440c0cb5d90353f4f61a9e
Roverp <yar.zubaha@proton.me>
2025-11-02 13:28:46 +0100

    fix: delete useWindowWidth hook which caused hydration error

--- frontend/src/widgets/hero-section/ui/description-block/description-block.tsx
+++ frontend/src/widgets/hero-section/ui/description-block/description-block.tsx
@@ -2,30 +2,17 @@ import cn from "classnames";
 
 import ButtonStartProject from "@shared/ui/buttons/ButtonStartProject/ButtonStartProject";
 
-import { useWindowWidth } from "@shared/lib";
-
 import s from "./description-block.module.scss";
 
 export const DescriptionBlock = () => {
-  const windowWidth = useWindowWidth();
-
   return (
     <div className={cn(s.descriptionBlock, "Container")}>
       <p>Ми створимо Вам сайт для будь-яких ваших цілей!</p>
 
-      {windowWidth < 380 && (
-        <ButtonStartProject
-          text="Отримати консультацію"
-          className={s.button}
-        />
-      )}
-
-      {windowWidth >= 380 && (
-        <ButtonStartProject
-          text="Отримати безкоштовну консультацію"
-          className={s.button}
-        />
-      )}
+      <ButtonStartProject
+        text="Отримати безкоштовну консультацію"
+        className={s.button}
+      />
     </div>
   );
 };
