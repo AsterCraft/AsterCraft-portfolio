7d8c5937643c952f130bead52bbaa3037cd4de23
Roverp <yar.zubaha@proton.me>
2025-11-06 11:34:01 +0100

    feat(routes): define /uk/contact/ route

--- frontend/src/app/routes.ts
+++ frontend/src/app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, route, index } from "@react-router/dev/routes";
 export default [
   index("./routes/_index.tsx"),
   route("uk", "./routes/uk._index.tsx"),
+  route("uk/contact", "./routes/uk.contact.tsx"),
 ] satisfies RouteConfig;
