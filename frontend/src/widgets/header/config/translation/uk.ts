f88c201bf34edd9279386f63581916ae8fc13bce
Roverp <yar.zubaha@proton.me>
2025-11-14 18:33:17 +0100

    feat(header): initial rewrite of the header to md3

---
+++ frontend/src/widgets/header/config/translation/uk.ts
@@ -0,0 +1,9 @@
+export const headerTranslationUk = {
+  brandName: "AsterCraft",
+  buttonStartProject: "Замовити сайт",
+  nav: {
+    development: "Розробка сайтів",
+    portfolio: "Портфоліо",
+    contacts: "Контакти",
+  },
+} as const;
---
+++ frontend/src/widgets/header/model/i18n.ts
@@ -0,0 +1,4 @@
+import i18n from "@shared/i18n";
+import { headerTranslationUk } from "../config/translation/uk";
+
+i18n.addResourceBundle("uk", "header", headerTranslationUk);
