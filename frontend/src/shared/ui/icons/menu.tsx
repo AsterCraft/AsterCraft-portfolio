33dfd3422882d0af954e88c360c68226d9495ef3
Roverp <yar.zubaha@proton.me>
2025-11-17 11:49:03 +0100

    feat(icons): change icon fill to currentColor

--- frontend/src/shared/ui/icons/menu.tsx
+++ frontend/src/shared/ui/icons/menu.tsx
@@ -10,7 +10,7 @@ export const MenuIcon = ({ className }: IconProps) => {
       height="24px"
       viewBox="0 -960 960 960"
       width="24px"
-      fill="#e3e3e3"
+      fill="currentColor"
     >
       <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
     </svg>
