cf1ff22415311c083b3ab81813410ac5d6993608
Roverp <yar.zubaha@proton.me>
2025-11-21 13:18:04 +0100

    feat(widgets): init services-section

---
+++ frontend/src/widgets/services-section/config/translation/index.ts
@@ -0,0 +1,7 @@
+import { servicesSectionTranslationEn } from "./en.ts";
+import { servicesSectionTranslationUk } from "./uk.ts";
+
+export default {
+  en: servicesSectionTranslationEn,
+  uk: servicesSectionTranslationUk,
+} as const;
