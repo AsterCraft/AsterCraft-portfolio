cd1e82ff8b67067d59dd53ecfab5d600e4ccdd38
Roverp <yar.zubaha@proton.me>
2025-11-21 13:18:04 +0100

    feat(widgets): init services-section

---
+++ frontend/src/widgets/services-section/config/translation/en.ts
@@ -0,0 +1,40 @@
+export const servicesSectionTranslationEn = {
+  title: "Our specialization",
+
+  tabs: [
+    {
+      id: "web-development",
+      label: "Web Development",
+    },
+    {
+      id: "digital-marketing",
+      label: "Digital Marketing",
+    },
+  ],
+
+  services: {
+    "web-development": [
+      {
+        title: "Landing Page",
+      },
+      {
+        title: "E-commerce",
+      },
+      {
+        title: "Corporate Websites",
+      },
+    ],
+
+    "digital-marketing": [
+      {
+        title: "SEO",
+      },
+      {
+        title: "Google Ads",
+      },
+      {
+        title: "Targeted advertising",
+      },
+    ],
+  },
+} as const;
