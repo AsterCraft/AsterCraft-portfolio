cf5a1b849e01efaae577d9296ceea3cbdba564fd
Roverp <yar.zubaha@proton.me>
2025-11-24 18:11:57 +0100

    feat(seo-section): get seo-text from child props

--- frontend/src/pages/home/ui/home.tsx
+++ frontend/src/pages/home/ui/home.tsx
@@ -1,4 +1,5 @@
 import cn from "classnames";
+import { useTranslation } from "react-i18next";
 
 import { SectionAboutUs } from "@widgets/section-about-us";
 import SectionPromise from "@widgets/sections/SectionPromise/ui/SectionPromise";
@@ -11,11 +12,13 @@ import SectionTelegramContact from "@widgets/sections/SectionTelegramContact/ui/
 import { ConsultationSection } from "@widgets/consultation-section";
 import { FAQSection } from "@widgets/faq-section";
 import { ServicesSection } from "@widgets/services-section";
-
-import s from "./home.module.scss";
 import { SeoSection } from "@widgets/seo-section";
 
+import s from "./home.module.scss";
+
 export const PageHome = () => {
+  const { t } = useTranslation("homePage");
+
   return (
     <main>
       <HeroSection />
@@ -42,7 +45,59 @@ export const PageHome = () => {
       <SectionProjects />
 
       <ConsultationSection />
-      <SeoSection />
+      <SeoSection>
+        <h1>{t("seo.heading")}</h1>
+
+        <p>{t("seo.intro.paragraph1")}</p>
+        <p>{t("seo.intro.paragraph2")}</p>
+
+        <h2>{t("seo.typesOfSites.heading")}</h2>
+
+        <p>{t("seo.typesOfSites.intro")}</p>
+
+        <ul>
+          {t("seo.typesOfSites.items", { returnObjects: true }).map(
+            (item, i) => (
+              <li key={i}>{item}</li>
+            )
+          )}
+        </ul>
+
+        <p>{t("seo.typesOfSites.conclusion")}</p>
+
+        <h2>{t("seo.turnkeyCreation.heading")}</h2>
+
+        <p>{t("seo.turnkeyCreation.intro")}</p>
+
+        <p>{t("seo.turnkeyCreation.activitiesIntro")}</p>
+
+        <ul>
+          {t("seo.turnkeyCreation.activities", { returnObjects: true }).map(
+            (activity, i) => (
+              <li key={i}>{activity}</li>
+            )
+          )}
+        </ul>
+
+        <p>{t("seo.turnkeyCreation.conclusion")}</p>
+
+        <h2>{t("seo.costFactors.heading")}</h2>
+
+        <p>{t("seo.costFactors.intro")}</p>
+
+        <ul>
+          {t("seo.costFactors.factors", { returnObjects: true }).map(
+            (factor, i) => (
+              <li key={i}>{factor}</li>
+            )
+          )}
+        </ul>
+
+        <p>{t("seo.costFactors.conclusion")}</p>
+
+        <h2>{t("seo.whyUs.heading")}</h2>
+        <p>{t("seo.whyUs.content")}</p>
+      </SeoSection>
 
       <DividerBetweenSections />
       <TestimonialsSlider />
--- frontend/src/widgets/seo-section/ui/seo-section.tsx
+++ frontend/src/widgets/seo-section/ui/seo-section.tsx
@@ -9,7 +9,11 @@ import { MailIcon } from "@shared/ui";
 import s from "./seo-section.module.scss";
 import gs from "@shared/styles/global.module.scss";
 
-const SeoSection = () => {
+type SeoSectionProps = {
+  children: React.ReactNode;
+};
+
+const SeoSection = ({ children }: SeoSectionProps) => {
   const [selectedCountry, setSelectedCountry] = useState(false);
 
   return (
@@ -99,150 +103,7 @@ const SeoSection = () => {
           />
         </div>
 
-        <div className={cn(s.textColumn, s.column)}>
-          <h1>Creating websites in Ukraine</h1>
-
-          <p>
-            Website development - a complex process, which involves many
-            IT-specialists of different profiles. Today there is practically no
-            company that has not "brought" its business online. The development
-            and launch of a web resource is carried out in several stages. Only
-            thorough preparation makes it possible to implement a project that
-            will bring results to the business. Therefore, the choice of the
-            contractor should be taken responsibly.
-          </p>
-
-          <p>
-            Our web studio in Kiev MEGASITE develops web-sites, e-commerce
-            platforms, turnkey corporate resources for different business niches
-            and any level of complexity.
-          </p>
-
-          <h2>Types of sites</h2>
-
-          <p>
-            Website development in Ukraine is a demanded type of IT-service.
-            Virtually any line of business needs to create their own web
-            resource, as customers are often much more convenient to interact
-            with the company in online format. The cost of the project depends
-            on the type of site, the complexity of the work, and the niche in
-            which the client operates. The following varieties of WEB-resources
-            are in demand:
-          </p>
-
-          <ul>
-            <li>
-              Business card - the site is suitable for representatives of small
-              businesses who want to introduce potential customers to their
-              company. Typically, this resource contains 3-5 pages and gives a
-              general idea of the services and products;
-            </li>
-            <li>
-              Corporate - multi-page website, which describes products/services
-              of business in details, has extended functionality and features.
-              It may also be intended for internal use by employees;
-            </li>
-            <li>
-              Internet-shop - the trading platform, which allows you to offer
-              your goods or services to a greater number of customers. Payment
-              tools may be connected, possibility to create personal accounts
-              and other useful functions;
-            </li>
-            <li>
-              landing page - a single-page page created for advertising
-              purposes. It allows you to increase conversion through a call to
-              take a specific action, which is necessary for business;
-            </li>
-            <li>
-              Online services - platforms that provide services (registration,
-              ticketing, online registration, and so on).
-            </li>
-          </ul>
-
-          <p>
-            If you are interested in developing web sites in Ukraine, apply to
-            our IT company. We will help to solve IT-tasks of any complexity.
-          </p>
-
-          <h2>Turnkey website creation</h2>
-
-          <p>
-            Website development is a service of a full cycle of site
-            development, starting from market research and design of the
-            resource to the launch of the project and its subsequent
-            maintenance, advertising settings, SEO, etc. In this case, the
-            promotion is not always included in the price of the order.
-          </p>
-
-          <p>WEB-site development includes the following activities:</p>
-
-          <ul>
-            <li>
-              Market analysis and analysis of competitors, the study of the
-              client's business and research the target audience who will use
-              the resource;
-            </li>
-            <li>
-              Planning and detailed design of the project structure based on
-              these data;
-            </li>
-            <li>
-              creating a prototype of the project, its discussion and approval;
-            </li>
-            <li> drawing design;</li>
-            <li>layout and programming;</li>
-            <li>testing;</li>
-            <li>optimization and filling with content;</li>
-            <li>Final adjustments and delivery.</li>
-          </ul>
-
-          <p>
-            These are the main stages that performs our company in the creation
-            of the site. But if you want the customer can order the setting of
-            advertising, promotion and so on. Also, when the project is handed
-            over, we teach our client's employees to use the interface and site
-            administration.
-          </p>
-
-          <h2>What affects the cost of website development</h2>
-
-          <p>
-            Most often, before order website development, customers are
-            interested in how much it will cost this service. You can find out
-            the price after a conversation with a manager. The budget of the
-            project may vary due to many factors:
-          </p>
-
-          <ul>
-            <li>The type of site that interests the customer;</li>
-            <li>the estimated number of pages;</li>
-            <li>the desired set of features;</li>
-            <li>time for the development of the project;</li>
-            <li>
-              the number and qualifications of the specialists involved in the
-              development.
-            </li>
-          </ul>
-
-          <p>
-            On average it takes 1-4 months to create a website. Depending on the
-            objectives of the project, we apply both template and custom
-            solutions. For example, the design can be standard, while the
-            functionality is developed specially for the web resource.
-          </p>
-
-          <h2>Why website creation is better to trust MEGASITE agency</h2>
-
-          <p>
-            Our IT company has extensive experience in creating and promoting
-            web resources of various complexity. We take into consideration
-            customer requirements and offer optimum solutions. Our projects are
-            effective and allow businesses to achieve their goals. We have
-            experienced specialists who can run even the most complex project.
-            Your WEB-resource will become a powerful basis for the promotion and
-            development of the company.
-          </p>
-        </div>
+        <div className={cn(s.textColumn, s.column)}>{children}</div>
       </div>
     </section>
   );
