b4ee6dc9b1246490f6e6ec8354d8bbd172253b57
Roverp <yar.zubaha@proton.me>
2025-11-26 19:09:47 +0100

    feat(app): init globalEnSchemas

---
+++ frontend/src/app/config/seo/en/website-schema.ts
@@ -0,0 +1,19 @@
+import { SITE_URL } from "@shared/config";
+
+/* @todo Translate this to english */
+const webSiteSchema = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "@id": `${SITE_URL}/#website`,
+
+  url: SITE_URL,
+  name: "AsterCraft",
+  description: "Професійна розробка веб-сайтів в Україні",
+  inLanguage: "uk",
+
+  publisher: {
+    "@id": `${SITE_URL}/#organization`,
+  },
+} as const;
+
+export default webSiteSchema;
