75a334c950e3028838f23d68187622ef40927f08
Roverp <yar.zubaha@proton.me>
2025-11-26 21:58:42 +0100

    refactor(app): _index.tsx -> /index.tsx route

--- frontend/src/app/routes.ts
+++ frontend/src/app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, route, index } from "@react-router/dev/routes";
 
 export default [
-  index("./routes/_index.tsx"),
+  index("./routes/index.tsx"),
   route("uk", "./routes/uk/index.tsx"),
   route("en", "./routes/en/index.tsx"),
   route("api/locales/:lng/:ns", "./routes/locales.ts"),
