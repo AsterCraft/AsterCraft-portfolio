85f0b514799be949557ee30ceb189f1179b55afa
Andrii Kovalenko <andriyworkkovalenko@gmail.com>
2025-12-09 19:04:09 +0200

    feat:new-rail

---
+++ frontend/src/widgets/header/context/RailsStateContext.tsx
@@ -0,0 +1,22 @@
+// RailsStateContext.tsx
+import { createContext, useState, type ReactNode } from 'react';
+
+interface RailStateContextType {
+  activeRailId: number;
+  setActiveRailId: (id: number) => void;
+}
+
+export const RailStateContext = createContext<RailStateContextType>({
+  activeRailId: 0,
+  setActiveRailId: () => {}
+});
+
+export default function RailStateProvider({ children }: { children: ReactNode }) {
+  const [activeRailId, setActiveRailId] = useState(0);
+
+  return (
+    <RailStateContext.Provider value={{ activeRailId, setActiveRailId }}>
+      {children}
+    </RailStateContext.Provider>
+  );
+}
