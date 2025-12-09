f663975fd0bfd2a3f9c29d93a2011d7cbb92b35f
Roverp <yar.zubaha@proton.me>
2025-12-09 15:23:54 +0100

    feat(project-sectino): fill up translation objects

--- frontend/src/widgets/projects-section/config/translations/uk.ts
+++ frontend/src/widgets/projects-section/config/translations/uk.ts
@@ -1,5 +1,36 @@
 import type { Translation } from "./en";
 
-const uk = {} as const;
+const uk = {
+  title: "Our projects",
+
+  projects: [
+    {
+      url: "https://www.roxy.com/",
+      img: "/img/projects/roxy.png",
+      name: "Roxy",
+      description: "Величезний магазин для спортивних товарів",
+    },
+    {
+      url: "https://www.uniprint.lviv.ua/",
+      img: "/img/projects/uni-print.png",
+      name: "Uni Print",
+      description:
+        "Багатосторінковий сайт-галерея з конструктором товарів для візуальної зовнішньої реклами",
+    },
+    {
+      url: "https://www.laura-lewandowski.com/",
+      img: "/img/projects/smart-chiefs.png",
+      name: "Smart Chiefs",
+      description:
+        "Сайт для Smart Chiefs яка інтегрує штучний інтелект в рекламу в Німеччині",
+    },
+    {
+      url: "https://www.odtahvozidel-24.cz/",
+      img: "/img/projects/odtahvozidel-24.png",
+      name: "Odtahvozidel-24",
+      description: "Односторіноковий сайт для послуг евакуатора в Чехії",
+    },
+  ],
+} as const satisfies Translation;
 
 export default uk;
