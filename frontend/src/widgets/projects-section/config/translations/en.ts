45bf19c3322b062ba7de7a9ca241418331f4ea21
Roverp <yar.zubaha@proton.me>
2025-12-11 17:58:44 +0100

    chore(projects-section): restructure translatinos

--- frontend/src/widgets/projects-section/config/translations/en.ts
+++ frontend/src/widgets/projects-section/config/translations/en.ts
@@ -10,20 +10,20 @@ const en = {
       name: "Roxy",
       description: "Величезний магазин для спортивних товарів",
     },
-    {
-      url: "https://www.uniprint.lviv.ua/",
-      img: "/img/projects/uni-print.png",
-      name: "Uni Print",
-      description:
-        "Багатосторінковий сайт-галерея з конструктором товарів для візуальної зовнішньої реклами",
-    },
-    {
-      url: "https://www.laura-lewandowski.com/",
-      img: "/img/projects/smart-chiefs.png",
-      name: "Smart Chiefs",
-      description:
-        "Сайт для Smart Chiefs яка інтегрує штучний інтелект в рекламу в Німеччині",
-    },
+    // {
+    //   url: "https://www.uniprint.lviv.ua/",
+    //   img: "/img/projects/uni-print.png",
+    //   name: "Uni Print",
+    //   description:
+    //     "Багатосторінковий сайт-галерея з конструктором товарів для візуальної зовнішньої реклами",
+    // },
+    // {
+    //   url: "https://www.laura-lewandowski.com/",
+    //   img: "/img/projects/smart-chiefs.png",
+    //   name: "Smart Chiefs",
+    //   description:
+    //     "Сайт для Smart Chiefs яка інтегрує штучний інтелект в рекламу в Німеччині",
+    // },
     {
       url: "https://www.odtahvozidel-24.cz/",
       img: "/img/projects/odtahvozidel-24.png",
