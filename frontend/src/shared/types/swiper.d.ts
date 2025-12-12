1290f957ab1392a2641021944ee1c14c2576f769
Roverp <yar.zubaha@proton.me>
2025-12-12 16:55:50 +0100

    chore(npm): rm swiper

--- frontend/src/shared/types/swiper.d.ts
+++
@@ -1,6 +0,0 @@
-// swiper.d.ts
-declare module 'swiper/css' {}
-declare module 'swiper/css/pagination' {}
-declare module 'swiper/css/autoplay' {}
-declare module 'swiper/css/effect-fade' {}
-declare module 'swiper/css/navigation' {}
