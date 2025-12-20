d894b6206e5fb2416c0d71ddd1656feb535f0e9b
Roverp <yar.zubaha@proton.me>
2025-12-20 22:04:28 +0100

    chore(footer): rm deprecated components

--- frontend/src/widgets/footer/ui/animated-brand-name/animated-brand-name.tsx
+++
@@ -1,7 +0,0 @@
-import s from "./animated-brand-name.module.scss";
-
-const AnimatedBrandName = () => {
-  return <h2 className={s.brandName}>ASTERCRAFT</h2>;
-};
-
-export default AnimatedBrandName;
