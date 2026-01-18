4d0daabb73c8660083ec85f1e31c5d2b2ca924ad
Roverp <yar.zubaha@proton.me>
2026-01-18 16:57:41 +0100

    feat(consultation-section): improve structure, consistency & styling

--- frontend/src/widgets/consultation-section/ui/consultation-section.tsx
+++ frontend/src/widgets/consultation-section/ui/consultation-section.tsx
@@ -1,27 +1,46 @@
-import { useTranslation } from "react-i18next";
-import s from "./consultation-section.module.scss";
-import DottedListItem from "@shared/ui/lists/dotted-list-item/DottedListItem";
-import gs from "@shared/styles/global.module.scss";
 import cn from "classnames";
+import { useTranslation } from "react-i18next";
+
+import DottedListItem from "@shared/ui/lists/dotted-list-item/DottedListItem";
+
+import s from "./consultation-section.module.scss";
+import gs from "@shared/styles/global.module.scss";
+
 const ConsultationSection = () => {
   const { t } = useTranslation("consultationSection");
 
   return (
-    <section 
-     className={s.main}
-    aria-labelledby="promise-heading"
+    <section
+      className={s.consultationSection}
+      aria-labelledby="consultationSection__heading"
     >
       <div className={cn(s.wrapper, gs.container)}>
-      <h2 id="promise-heading" className={s.title}>{t("heading")}</h2>
-      <div className={s.downTitleBlock}>
-        <span className={s.subtitle}>{t("infoForm.heading")}</span>
-        <ul className={s.listItem}>
-          <DottedListItem children={t("infoForm.first")} />
-          <DottedListItem children={t("infoForm.double")} />
-          <DottedListItem children={t("infoForm.third")} />
-        </ul>
-        <div className={s.downText}>{t("comment")}</div>
-      </div>
+        <h2
+          id="consultationSection__heading"
+          className={s.title}
+        >
+          {t("heading")}
+        </h2>
+
+        <div className={s.description}>
+          <h3
+            className={s.subtitle}
+            id="consultationSection__subheading"
+          >
+            {t("infoForm.heading")}
+          </h3>
+
+          <ul
+            className={s.list}
+            aria-labelledby="consultationSection__subheading"
+          >
+            <DottedListItem>{t("infoForm.first")}</DottedListItem>
+            <DottedListItem>{t("infoForm.double")}</DottedListItem>
+            <DottedListItem>{t("infoForm.third")}</DottedListItem>
+          </ul>
+        </div>
+
+        <p className={s.downText}>{t("comment")}</p>
       </div>
     </section>
   );
