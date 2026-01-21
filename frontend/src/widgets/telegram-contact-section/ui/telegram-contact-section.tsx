13b79f08036164cfe92aa9a8e3723b4d43dd4611
Roverp <yar.zubaha@proton.me>
2026-01-21 22:29:43 +0100

    feat(telegram-contact-section): write & style

--- frontend/src/widgets/telegram-contact-section/ui/telegram-contact-section.tsx
+++ frontend/src/widgets/telegram-contact-section/ui/telegram-contact-section.tsx
@@ -1,9 +1,35 @@
+import cn from "classnames";
+
+import { Link } from "react-router";
+import { TelegramLogo } from "@shared/ui";
+
 import s from "./telegram-contact-section.module.scss";
 
-const TelegramContactSection = () => {
+const TelegramContactSection = ({
+  className,
+  ...props
+}: React.ComponentPropsWithRef<"section">) => {
   return (
-    <section>
-      <div></div>
+    <section
+      className={cn(s.telegramContactSection, className)}
+      aria-labelledby="telegramContactSection__heading"
+      {...props}
+    >
+      <h2
+        className={s.heading}
+        id="telegramContactSection__heading"
+      >
+        <span>Have a question?</span>
+        <span>Ask us in Telegram:</span>
+      </h2>
+
+      <Link
+        to="https://t.me/AsterCraft"
+        className={s.telegramLink}
+      >
+        <TelegramLogo className={s.telegramLogo} />
+        <span className={s.at}>@AsterCraft</span>
+      </Link>
     </section>
   );
 };
